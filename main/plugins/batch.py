f7e335ef5b7cda515bc28cee9e34811462076611
selfstudyhans <selfstudyhansraj@gmail.com>
2024-06-18 14:38:43 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -215,7 +215,7 @@ async def _bulk(event):
             save_batch_data(batch_data)
 
             cd = await conv.send_message("**Batch process ongoing...**\n\nProcess completed: ", 
-                                    buttons=[[Button.url("Join Channel", url="http://t.me/Mahi_batches")]])
+                                    buttons=[[Button.url("Join Channel", url="http://t.me/onlinehackingxx")]])
             co = await r_batch(userbot, Bot, user_id, cd, _link) 
             try: 
                 if co == -2:
@@ -262,7 +262,7 @@ async def r_batch(userbot, client, sender, countdown, link):
             await get_bulk_msg(userbot, client, sender, link, integer)
             protection = await client.send_message(sender, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
             await countdown.edit(count_down, 
-                                 buttons=[[Button.url("Join Channel", url="https://t.me/mahi_batches")]])
+                                 buttons=[[Button.url("Join Channel", url="https://t.me/onlinehackingxx")]])
             await asyncio.sleep(timer)
             await protection.delete()
         except IndexError as ie:
@@ -284,12 +284,12 @@ async def r_batch(userbot, client, sender, countdown, link):
                 except Exception as e:
                     logger.info(e)
                     if countdown.text != count_down:
-                        await countdown.edit(count_down, buttons=[[Button.url("Join Channel", url="http://t.me/devggn")]])
+                        await countdown.edit(count_down, buttons=[[Button.url("Join Channel", url="http://t.me/onlinehackingxx")]])
         except Exception as e:
             #logger.info(e)
             #await client.send_message(sender, f"An error occurred during cloning, batch will continue.\n\n**Error:** {str(e)}")
             if countdown.text != count_down:
-                await countdown.edit(count_down, buttons=[[Button.url("Join Channel", url="https://t.me/devggn")]])
+                await countdown.edit(count_down, buttons=[[Button.url("Join Channel", url="https://t.me/onlinehackingxx")]])
         n = i + 1
         if n == len(ids_data[str(sender)]):
             return -2
