a9249ac9ce143b9e9e752fe0f8b76d0438a0a63c
selfstudyhans <selfstudyhansraj@gmail.com>
2024-06-18 14:30:08 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -35,7 +35,7 @@ userbot = Client("myacc",api_id=API_ID,api_hash=API_HASH,session_string=SESSION)
 try:
     userbot.start()
 except BaseException:
-    print("Your session expired please re add that... thanks @dev_gagan.")
+    print("Your session expired please re add that... thanks @owner_bhalu.")
     sys.exit(1)
 
 Bot = Client(
